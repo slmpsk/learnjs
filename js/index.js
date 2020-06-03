6a5a8fca8990249fb45929bfef7314c03fb0cbbe
slmpsk <65248597+slmpsk@users.noreply.github.com>
2020-06-03 16:49:34 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,39 +1,30 @@
-https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript?fbclid=IwAR21B5FXrhWl89gKVBUKV8KM63U4fO0Kt56rmfKtxDvxjdz74ZCIGuim-qU
-function greet (name, owner) {
-    if(name === owner) {
-        return('Hello boss')
-    } else {
-        return('Hello guest')
-    };
-}
-https://www.codewars.com/kata/keep-hydrated-1/train/javascript?fbclid=IwAR0dPVxIlesVfK_RugmiK_Lw-WBQ3soyZKUxp7DNcRkO_lhBBrWKhJNCRTg
-function litres(time,a){
-    a=time/2;
-    time= a%1;
-    a=a-time;
-    return a;
-    return 0;
-};
-https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript?fbclid=IwAR0-j6U-54o2hwoqKWZAy9vIVduY2CkL89hGDIYtxXz1TiFMos73GjbxE0Q
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return laLigaGoals+copaDelReyGoals+championsLeagueGoals
-};
-https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript?fbclid=IwAR14on_cdMP7Q_qiQ9CgupE7ra1kFo3hY7KAVUaVdS4vBpg9ayak1H_YT08
-function move (position, roll) {
-  return position+roll*2;
-}
-https://www.codewars.com/kata/be-concise-i-the-ternary-operator/train/javascript?fbclid=IwAR2JvkPql5kSbKdPg2z7Mm0O7pteSJab5hd35Qb5Vf4-yi--rTxcAmbRN_4
-function describeAge(age,str) {
-  str="You're a(n)"
-  if (age <= 12) {
-    return str+" kid";
-  } else if (age >= 13 && age <= 17) {
-    return str+" teenager";
-  } else if (age >= 18 && age <= 64) {
-    return str+" adult";
-  } else {
-    return str+" elderly";
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR111iMluJ8yI_5UwXxhjIiH0btGGTpAY_-YVoayyN_KRWedFYN1qHoG624
+
+class SmallestIntegerFinder {
+  findSmallestInt(args){
+    console.log("Should return the smallest int "+Math.min(...args));
+    return Math.min(...args);
   }
-};
+}
 
+https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR0-x3L3wsblRq46qTdWm5kKareFqLugtBWtayGx8kwDZQfGzzCW5WZLWIw
 
+unction circleCircumference(circle) {
+ return (2 * Math.PI*circle.radius);
+}
+
+https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript?fbclid=IwAR24lhYLnSlvDf3qnl6dAA_iDUHg6JjvkmqA4cesm8bex3CEVxHFWUFJFdk
+
+function giveMeFive(obj){
+let newObj=[];
+ for (var key in obj){
+    if (key.length===5){
+       newObj.push(key);
+       };
+    if (obj[key].length===5){
+       newObj.push(obj[key]);
+       };
+       }
+
+return newObj; 
+}
